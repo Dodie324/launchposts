2f013be5faaf36fbb1c03350e83f03c2f3c39b6f
Dodie324 <drmoore324@gmail.com>
2015-03-04 14:46:00 -0500

    saving work before making new changes

--- schema.sql
+++ schema.sql
@@ -1,14 +1,22 @@
-DROP TABLE IF EXISTS ruby_posts, html_css_posts, javascript_posts, misc, posts, languages;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS web_development;
 
-CREATE TABLE languages (
-  id serial PRIMARY KEY,
-  language VARCHAR(50) NOT NULL
+CREATE TABLE web_development (
+  id SERIAL PRIMARY KEY,
+  topic VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE posts (
-  id serial PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  url VARCHAR(255) UNIQUE,
-  description VARCHAR(255) NOT NULL,
-  language_id int REFERENCES languages(id) NOT NULL
+  url VARCHAR(500) UNIQUE,
+  description VARCHAR(500) NOT NULL,
+  post_date DATE,
+  language_id INT REFERENCES languages(id) NOT NULL,
 );
+
+INSERT INTO web_development (topic) VALUES ('Ruby');
+INSERT INTO web_development (topic) VALUES ('HTML/CSS');
+INSERT INTO web_development (topic) VALUES ('JavaScript/jQuery');
+INSERT INTO web_development (topic) VALUES ('SQL');
+INSERT INTO web_development (topic) VALUES ('Misc');
--- server.rb
+++ server.rb
@@ -13,15 +13,27 @@ def db_connection
 end
 
 def add_new_post(params)
-  post = [params["title"], params["url"], params["description"]]
+  post = [params["topic_id"], params["title"], params["url"], params["description"]]
   db_connection do |conn|
-    conn.exec_params( "INSERT INTO posts (id, title, url, description, language_id) VALUES (DEFAULT, $1, $2, $3, 1)", post )
+    conn.exec_params( "INSERT INTO posts (topic_id, title, url, description) 
+                       VALUES ($1, $2, $3, $4)", post )
   end
+end
+
+def display_post(id)
+  db_connection { |conn| conn.exec( "SELECT title, url, description
+                                     FROM posts
+                                     WHERE topic_id = #{id}
+                                     ORDER BY post_date DESC" ) }
 end
 
 ##########
 # ROUTES #
 ##########
+
+get '/' do
+  redirect '/posts'
+end
 
 get '/posts' do
   erb :index
@@ -29,6 +41,37 @@ end
 
 get '/posts/new' do
   erb :new
+end
+
+get '/posts/:topic' do
+
+  erb :show, locals: { task_name: params[:task_name] }
+end
+
+
+get '/posts/ruby' do
+  ruby_posts = display_post(1)
+  erb :ruby, locals: { ruby: ruby_posts }
+end
+
+get '/posts/html_css' do
+  html_css_posts = display_post(2)
+  erb :html_css, locals: { html_css: html_css_posts }
+end
+
+get '/posts/javascript_jquery' do
+  javascript_jquery_posts = display_post(3)
+  erb :javascript_jquery, locals: { javascript_jquery: javascript_jquery_posts }
+end
+
+get '/posts/sql' do
+  sql_posts = display_post(4)
+  erb :sql, locals: { sql: sql_posts }
+end
+
+get '/posts/misc' do
+  misc_posts = display_post(5)
+  erb :misc, locals: { misc: misc_posts }
 end
 
 post '/posts/new' do
